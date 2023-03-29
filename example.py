47719b2ee1da51f5f9c042fa9b300754575fb322
samuele <ruggerisamuele00@gmail.com>
2023-03-29 13:09:05 +0200

    Aggiunti esempi per i nuovi metodi

--- example.py
+++ example.py
@@ -2,9 +2,14 @@ from Entity import Entity
 
 # Esempio: genera un oggetto Entity da un entity_id in input
 entity = Entity("https://annis.fdm.uni-hamburg.de/shibboleth")
-print("Description: ", entity.get_dsc())
-print("Organization: ", entity.get_org())
+print("Descrizione: ", entity.get_dsc())
+print("Controlla se esiste una descrizione in italiano: ")
+print(entity.get_dsc_in('it'))
+print("Organizzazione: ", entity.get_org())
 
+# Esempio: controlla se l'entità è erogata da un'rganizzazione italiana
+italian = Entity("https://sp-bestr-prod.cineca.it/shibboleth")
+print("L'entità è erogata da un'organizzazione italiana? ", italian.is_italian())
 
 
 # Esempio: genera un oggetto Entity da un row_id in input
