7f733b0f7a56bd317340d4ebc7cf8165bb4dca63
samuele <ruggerisamuele00@gmail.com>
2023-03-29 13:14:31 +0200

    Aggiunti esempio per il metodo get_org_url()

--- example.py
+++ example.py
@@ -3,9 +3,10 @@ from Entity import Entity
 # Esempio: genera un oggetto Entity da un entity_id in input
 entity = Entity("https://annis.fdm.uni-hamburg.de/shibboleth")
 print("Descrizione: ", entity.get_dsc())
-print("Controlla se esiste una descrizione in italiano: ")
-print(entity.get_dsc_in('it'))
+print("Controlla se esiste una descrizione in italiano: ", entity.get_dsc_in('it'))
 print("Organizzazione: ", entity.get_org())
+print("URL dell'organizzazione: ", entity.get_org_url())
+
 
 # Esempio: controlla se l'entità è erogata da un'rganizzazione italiana
 italian = Entity("https://sp-bestr-prod.cineca.it/shibboleth")
