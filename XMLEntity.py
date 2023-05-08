47726fa06f23709da9664bb82544a852ba3d24ba
samuele <ruggerisamuele00@gmail.com>
2023-05-08 13:51:11 +0200

    Aggiunti dei metodi

--- XMLEntity.py
+++ XMLEntity.py
@@ -5,11 +5,15 @@ import time
 class XMLEntity:
 
     # Il costruttore parsa il file XML di metadata.
-    def __init__(self):
+    def __init__(self, entity_id=None):
         self.tree = ET.parse("edugain-v2.xml")
         self.root = self.tree.getroot()
-        self.entity_ids = ""
+        if entity_id is not None:
+            self.entity_id = entity_id
+        self.entity_id = ""
+        self.regauth = ""
         self.dsc = ""
+        self.display_names = ""
 
     def get_all_entityids(self):
         entity_ids = []
@@ -18,13 +22,21 @@ class XMLEntity:
                 entity_ids.append(entity_id)
         return entity_ids
 
+    def get_entityid(self):
+        return self.entity_id
+
+    def get_all_regatuh(self):
+
+        regs = self.tree.findall(".//{urn:oasis:names:tc:SAML:metadata:rpi}RegistrationInfo")
+        regauth_list = [reg.get("registrationAuthority") for reg in regs]
+        reg_set = set(regauth_list)
+        return reg_set
+    """
     def get_dsc(self):
-        desc = self.tree.findall(".//{urn:oasis:names:tc:SAML:2.0:metadata}SPSSODescriptor")
-        if desc is not None:
-            for d in desc:
-                print(d.ET.SubElement())
-            #for child in sp:
-            #    print(child.text)
+        desc = self.tree.findall(".//{urn:oasis:names:tc:SAML:2.0:metadata:ui}Description")
+        for d in desc:
+            print(d.text)
+    """
 
     def get_lang(self, org_name):
         names = self.tree.findall(".//{urn:oasis:names:tc:SAML:2.0:metadata}OrganizationName")
@@ -37,7 +49,7 @@ class XMLEntity:
 
     def get_display_names(self):
         names = self.tree.findall(".//{urn:oasis:names:tc:SAML:2.0:metadata}OrganizationDisplayName")
-        display_names = set([name.text for name in names])
-        return display_names
-
+        self.display_names = set([name.text for name in names])
+        return self.display_names
+ù
 
